55a73608a98e21d6b64a68771c5639492291c531
Maksudul Haque <saad.mk112@gmail.com>
2020-09-02 21:16:27 +0600

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -4,6 +4,8 @@ echo ${GITHUB_REF}
 echo ${GITHUB_HEAD_REF}
 echo ${GITHUB_BASE_REF}
 
+git fetch origin
+git checkout ${GITHUB_HEAD_REF}
 git add .
 git commit -m "Added Changelog"
 git push origin HEAD
