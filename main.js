1699ffd781007a9d37d394cac97ae77311ea0113
Maksudul Haque <saad.mk112@gmail.com>
2021-11-15 20:17:43 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,3 @@
-var http = require("http");
-
-http.createServer(function (request, response) {
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   response.end('Hello World\n');
-}).listen(8081);
-
-console.log('Server running at http://127.0.0.1:8081/');
-
-
 const http = require('http'),
 const fs = require('fs');
 
